85371a00b0f5c0609c28bfb0af311da4dbdcae0c
yahya <m2i.abdellahyaya@gmail.com>
2019-01-18 17:06:09 +0100

    exemple

--- basesJava/src/main/java/fr/formation/inti/app/App2.java
+++ basesJava/src/main/java/fr/formation/inti/app/App2.java
@@ -3,8 +3,8 @@ package fr.formation.inti.app;
 public class App2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		System.out.println("test branche 2");
 	}
 
 }
